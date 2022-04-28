12c8596fa3d597015e7f248ed86e0d1331c99c96
godanfeng <103484507+godanfeng@users.noreply.github.com>
2022-04-28 20:52:15 +0800

    Delete SPH directory

--- SPH/src/utils/token.js
+++
@@ -1,5 +0,0 @@
-export const setToken=(token)=>{
-    localStorage.setItem('TOKEN',token)
-}
-export const getToken = () => localStorage.getItem('TOKEN')
-export const removeToken=()=>localStorage.removeItem('TOKEN')
